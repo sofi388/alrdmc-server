ae02ff6f5da52d82e4cacf4df861a1528dfc6c8a
Miika Matias <miikapiiparinen24@gmail.com>
2024-11-08 22:01:12 +0200

    change port for run script to 3308

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker stop mysql-db
 docker rm mysql-db
 docker build -t mysql-db -f ./images/mysql-db/Dockerfile .
-docker run --name mysql-db -p 3306:3306 -d mysql-db
+docker run --name mysql-db -p 3308:3306 -d mysql-db
 
 poetry install
 poetry run python3 src/app.py
