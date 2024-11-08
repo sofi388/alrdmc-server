42756231fece677e1565c4d952947cd9c45df487
Miika Matias <miikapiiparinen24@gmail.com>
2024-11-08 18:27:00 +0200

    simple working api

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+docker stop postgres
+docker rm postgres
 docker run --name postgres -e POSTGRES_PASSWORD=pass -d postgres
+poetry install
+poetry run python3 src/app.py
--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def index():  
     return "Hello World!"
 
