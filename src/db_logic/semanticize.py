34922315216c6e4096d7a457ad79f1be033628b0
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-11-09 10:33:56 +0000

    change to gemma

--- src/db_logic/semanticize.py
+++ src/db_logic/semanticize.py
@@ -13,24 +13,26 @@ tokenizer = AutoTokenizer.from_pretrained(
 model = AutoModel.from_pretrained(
     "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 )
-pipeline_generation_keywords = pipeline("text-generation", model='meta-llama/Llama-3.2-1B-Instruct', torch_dtype=torch.bfloat16, token=os.getenv("HF_TOKEN"))
+# pipeline_generation_keywords = pipeline("text-generation", model='meta-llama/Llama-3.2-1B-Instruct', torch_dtype=torch.bfloat16, token=os.getenv("HF_TOKEN"))
+# pipeline_generation_keywords = pipeline("text-generation", model='google/gemma-2-2b-it', torch_dtype=torch.bfloat16, token=os.getenv("HF_TOKEN"))
 
 
 def generate_semantic_vector(text: str):
     """
     To use this function, you can just call it with any text and it will return the semantic vector for that text
     """
-    # First, generate the keywords of the text
-    chat_template = [
-        {
-            "role": "system",
-            "content": "From the following text given by the user, extract a list of specific keywords related to the topic, separated by commas.",
-        },
-        {"role": "user", "content": text},
-    ]
-    generated_keywords = pipeline_generation_keywords(chat_template, max_new_tokens=100)[0]["generated_text"][-1]['content']
+    # # First, generate the keywords of the text
+    # chat_template = [
+    #     {
+    #         "role": "system",
+    #         "content": "From the following text given by the user, extract a list of specific keywords related to the topic, separated by commas.",
+    #     },
+    #     {"role": "user", "content": text[:500]},
+    # ]
+    # generated_keywords = pipeline_generation_keywords(chat_template, max_new_tokens=50)[0]["generated_text"][-1]['content']
 
-    print(f"Generated keywords: {generated_keywords}")
+    # print(f"Generated keywords: {generated_keywords}")
+    generated_keywords = text
 
     inputs = tokenizer(
         generated_keywords, return_tensors="pt", padding=True, truncation=True, max_length=512
