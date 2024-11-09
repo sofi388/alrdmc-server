aba9bcc477a5664c82944d0f71fd9976f8c480f1
Miika Matias <miikapiiparinen24@gmail.com>
2024-11-09 03:29:47 +0200

    refactoring

--- src/config/config.py
+++ src/config/config.py
@@ -4,3 +4,11 @@ PORT=5000
 POLLING_INTERVAL=1200
 
 EU_URL = "https://register.eci.ec.europa.eu/core/api/register/search/ALL/EN/0/20"
+
+CONNECTION_CONFIG = {
+    'host': "34.67.133.83",
+    'user': "root",
+    'port': "3308",
+    'password': "pass",
+    'database': "alrdmc"
+}
--- src/get_vectors.py
+++ src/get_vectors.py
@@ -2,15 +2,7 @@ import mysql.connector
 import numpy as np
 import json
 import ast
-
-# Database connection configuration
-connection_config = {
-    'host': "34.67.133.83",
-    'user': "root",
-    'port': "3308",
-    'password': "pass",
-    'database': "alrdmc"
-}
+from config.config import CONNECTION_CONFIG
 
 def parse_float_list(string):
     try:
@@ -19,7 +11,7 @@ def parse_float_list(string):
         return f"Error parsing string: {e}"
 
 def fetch_all_vectors():
-    connection = mysql.connector.connect(**connection_config)
+    connection = mysql.connector.connect(**CONNECTION_CONFIG)
     cursor = connection.cursor()
 
     query = "SELECT vector_data FROM initiatives WHERE vector_data IS NOT NULL"
@@ -36,78 +28,38 @@ def fetch_all_vectors():
 
     return vector_list
 
-
-
-
-
 def fetch_all_titles():
-    # Establish the connection
-    connection = mysql.connector.connect(**connection_config)
+    connection = mysql.connector.connect(**CONNECTION_CONFIG)
     cursor = connection.cursor()
 
-    # SQL query to fetch all non-null vectors
     query = "SELECT title FROM initiatives WHERE vector_data IS NOT NULL"
     cursor.execute(query)
     
-    # Initialize list to hold vectors
     title_list = []
     
     for (title_data,) in cursor.fetchall():
-        # Convert from JSON (or adjust if stored differently) to numpy array
         title = title_data
         title_list.append(title)
 
-    # Close database connection
     cursor.close()
     connection.close()
 
     return title_list
 
-
-
-
 def fetch_all_urls():
-    # Establish the connection
-    connection = mysql.connector.connect(**connection_config)
+    connection = mysql.connector.connect(**CONNECTION_CONFIG)
     cursor = connection.cursor()
 
-    # SQL query to fetch all non-null vectors
     query = "SELECT initiative_url FROM initiatives WHERE vector_data IS NOT NULL"
     cursor.execute(query)
     
-    # Initialize list to hold vectors
     url_list = []
     
     for (url_data,) in cursor.fetchall():
-        # Convert from JSON (or adjust if stored differently) to numpy array
         url = url_data
         url_list.append(url)
 
-    # Close database connection
     cursor.close()
     connection.close()
 
     return url_list
-
-
-
-
-
-# Retrieve all vectors
-semantic_vector_list = fetch_all_vectors()
-title_list = fetch_all_titles()
-url_list = fetch_all_urls()
-
-print(type(semantic_vector_list[0]))
-print(type(title_list[0]))
-print(type(url_list[0]))
-
-# Display results
-# print("All vectors retrieved from the initiative table.")
-# print(semantic_vector_list)
-
-# print("All titles retrieved from the initiative table.")
-# print(title_list)
-
-# print("All urls retrieved from the initiative table.")
-# print(url_list)
