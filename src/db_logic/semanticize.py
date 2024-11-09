a7b26e0cce06e9cba970f26ceb21956e89f1a5b6
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-11-09 02:38:49 +0000

    Update to generation of vectors

--- src/db_logic/semanticize.py
+++ src/db_logic/semanticize.py
@@ -2,18 +2,20 @@
 from transformers import AutoTokenizer, AutoModel
 
 tokenizer = AutoTokenizer.from_pretrained(
-    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    "Xenova/paraphrase-multilingual-MiniLM-L12-v2"
 )
-model = AutoModel.from_pretrained(
-    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-)
+model = AutoModel.from_pretrained("Xenova/paraphrase-multilingual-MiniLM-L12-v2")
+
 
 def generate_semantic_vector(text: str):
     """
     To use this function, you can just call it with any text and it will return the semantic vector for that text
     """
     inputs = tokenizer(
-            text, return_tensors="pt", padding=True, truncation=True, max_length=512
-        )
+        text, return_tensors="pt", padding=True, truncation=True, max_length=512
+    )
     outputs = model(**inputs)
-    return outputs.pooler_output.squeeze().tolist()
+    last_layer_states = outputs[0]
+    # Sum and divide by the number of tokens
+    pooled_output = last_layer_states.sum(axis=1).mul(1.0 / last_layer_states.shape[1])
+    return pooled_output.squeeze().tolist()
