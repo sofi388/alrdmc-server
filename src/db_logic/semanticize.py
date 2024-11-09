6812f63eb150b38462a64327661b69340b0f1cec
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-11-09 09:33:06 +0000

    generate semantic vectors based on prompts

--- src/db_logic/semanticize.py
+++ src/db_logic/semanticize.py
@@ -1,5 +1,6 @@
 # Load model directly
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, pipeline
+import torch
 
 tokenizer = AutoTokenizer.from_pretrained(
     "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
@@ -7,12 +8,16 @@ tokenizer = AutoTokenizer.from_pretrained(
 model = AutoModel.from_pretrained(
     "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 )
+pipeline_generation_keywords = pipeline("text-generation", model='meta-llama/Llama-3.2-1B-Instruct', torch_dtype=torch.bfloat16)
 
 
 def generate_semantic_vector(text: str):
     """
     To use this function, you can just call it with any text and it will return the semantic vector for that text
     """
+    # First, generate the keywords of the text
+    pipeline_generation_keywords("From the following text, extract a list of specific keywords related to the topic: " + text)
+
     inputs = tokenizer(
         text, return_tensors="pt", padding=True, truncation=True, max_length=512
     )
