36d92efa23d0286ed380f289d824949375a3f253
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-11-09 03:59:20 +0000

    Changes

--- src/db_logic/otakantaa_scraper.py
+++ src/db_logic/otakantaa_scraper.py
@@ -16,19 +16,23 @@ def fetch_otakantaa():
     # Convert the XML to a list of dictionaries.
     return_data = []
     for item in root.findall("./channel/item"):
-        return_data.append({
-            "title": item.find("title").text,
-            "url": item.find("link").text,
-            "description": item.find("description").text,
-        })
-
-    return_data = return_data[:5]
+        try:
+            return_data.append({
+                "title": item.find("title").text,
+                "url": item.find("link").text,
+                "description": item.find("description").text,
+            })
+        except:
+            print(f"Error parsing item: {item}")
 
     # The text is in finnish - we can use the `transformers` module to translate it to english.
     translator = transformers.pipeline("translation", model="Helsinki-NLP/opus-mt-fi-en")
     for item in return_data:
-        item["title_en"] = translator(item["title"])[0]["translation_text"]
-        item["description_en"] = translator(item["description"])[0]["translation_text"]
+        try:
+            item["title_en"] = translator(item["title"])[0]["translation_text"]
+            item["description_en"] = translator(item["description"])[0]["translation_text"]
+        except:
+            print(f"Error translating item: {item}")
     
     connection = mysql.connector.connect(
         host="34.67.133.83",
@@ -40,15 +44,23 @@ def fetch_otakantaa():
     
     # Get the semantic vectors for each title
     for item in return_data:
-        generate_vector_from = item["title_en"] + "\n" + item["description_en"]
-        item["vector"] = json.dumps(generate_semantic_vector(generate_vector_from))
+        try:
+            generate_vector_from = item["title_en"] + "\n" + item["description_en"]
+            item["vector"] = json.dumps(generate_semantic_vector(generate_vector_from))
+        except:
+            print(f"Error generating vector for item: {item}")
     
     cursor = connection.cursor()
     
     query = "INSERT INTO initiatives (title, initiative_url, vector_data, description) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE vector_data=VALUES(vector_data)"
 
     for item in return_data:
-        cursor.execute(query, (item["title_en"], item["url"], item["vector"], item["description_en"]))
+        if "title_en" not in item:
+            continue
+        try:
+            cursor.execute(query, (item["title_en"], item["url"], item["vector"], item["description_en"]))
+        except:
+            print(f"Error inserting item: {item}")
 
     return return_data
 
